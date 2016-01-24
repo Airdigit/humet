088b590f25eeb67978213e6671cc92944d13c22f
airdigit <airdigit.in@gmail.com>
2016-01-25 03:58:00 +0530

    mobile app init

---
+++ client/mapp/www/js/controllers.js
@@ -0,0 +1,18 @@
+angular.module('app.controllers', [])
+  
+.controller('loginCtrl', function($scope) {
+
+})
+   
+.controller('dashboardCtrl', function($scope) {
+
+})
+   
+.controller('cartTabDefaultPageCtrl', function($scope) {
+
+})
+   
+.controller('cloudTabDefaultPageCtrl', function($scope) {
+
+})
+    
