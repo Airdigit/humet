35c63a38eeb772f55e64d16a205fe52460fd9f1e
airdigit <airdigit.in@gmail.com>
2016-01-26 22:14:32 +0530

    Init

--- client/www/js/dev.js
+++
@@ -1,8 +0,0 @@
-angular.module('looper.dev', ['lbServices', 'ionic'])
-    .controller('DevCtrl', function ($scope, User, Avatar) {
-        /**
-         * Blank page for testing purposes
-         */
-        $scope.currentUser = User.getCachedCurrent()
-
-    });
